a281b07f6e352f3fe6ce41fff23248321962b102
chillpen <jedgbe@hotmail.com>
2015-07-20 07:06:34 +0800

    20150720-home
    07：06

---
+++ FireMonitor/FireMonitor/SHPOperator/ShpOperator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FireMonitor.SHPOperator
+{
+    public class ShpOperator
+    {
+        public bool ReadShpFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+                return false;
+
+
+            FileStream fs = File.OpenRead(fileName);
+
+            BinaryReader binReader = new BinaryReader(fs);
+
+            long length = fs.Length;
+
+            binReader.ReadBytes(36);
+
+            double xmin, ymin, xmax, ymax;
+
+            xmin = binReader.ReadDouble();
+            ymax = binReader.ReadDouble();
+            xmax = binReader.ReadDouble();
+            ymin = binReader.ReadDouble();
+
+
+            //int head = fs.r
+
+            return true;
+        }
+    }
+}
